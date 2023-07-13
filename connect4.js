7986472bae31f0123efe522b0c1afac584ff4c76
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-07-13 11:37:42 -0400

    added reset btn functionality to placeInTable func

--- connect4.js
+++ connect4.js
@@ -42,12 +42,17 @@ function makeHtmlBoard() {
     top.append(headCell);
   }
   htmlBoard.append(top);
-  // function makeResetBtn() {
-  //   const resetBtn = document.createElement("button");
-  //   resetBtn.innerText = "Reset";
-  //   resetBtn.append(document);
+
+  // function clickResetBtn() {
+  //   const resetBtn = document.querySelector("#resetBtn");
+  //   const gamePiece = document.getElementsByClassName("piece");
+  //   resetBtn.addEventListener("click", function (e) {
+  //     console.log("you clicked the reset btn");
+
+  //     gamePiece.remove();
+  //   });
   // }
-  // makeResetBtn();
+  // clickResetBtn();
   // TODO: add comment for this code
   //nested loops which are creating the remainder of the playing board. as long as y is less than 6, we increment y starting at 0.  for each loop create a table row.  then we have a nested loop for the columns.  as long as x is less than 7, we increment.  for each nested loop, we create a cell(td) which is being assigned an id of row number, which is set by the y loop-and x which is set by the x loop.  so 0-0, 0-1, 0-2, etc....  we are then appending the cells(td's) to the row(tr).  once the nest loops runs we are appending the rows to the htmlboard.
   for (let y = 0; y < HEIGHT; y++) {
@@ -83,6 +88,12 @@ function placeInTable(y, x) {
 
   playerSquare.append(gamePieceDiv);
   //appending the child,playerSquare to the parent element, gamePieceDiv
+  const resetBtn = document.querySelector("#resetBtn");
+  resetBtn.addEventListener("click", function (e) {
+    console.log("you clicked the reset btn");
+
+    gamePieceDiv.remove();
+  });
 }
 
 /** endGame: announce game end */
