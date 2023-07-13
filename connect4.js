ec4536332c789e4cf231858e9904ad3405e6493e
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-07-12 20:45:08 -0400

    completed handle click func, findSpotforCol func, and endGame func

--- connect4.js
+++ connect4.js
@@ -59,7 +59,12 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y; //return top empty row in column
+    }
+  }
+  return null; //return null if column is filled
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -68,16 +73,16 @@ function placeInTable(y, x) {
   const gamePieceDiv = document.createElement("div");
   gamePieceDiv.classList.add("piece");
   gamePieceDiv.classList.add(`player${currPlayer}`);
-  let lastTableRow = 5;
-  let column = x;
-  let startingPiece = document.getElementById(`${lastTableRow}-${column}`);
-  startingPiece.append(gamePieceDiv);
+  gamePieceDiv.style.top = -50 * (y + 2);
+  let playerSquare = document.getElementById(`${y}-${x}`);
+
+  playerSquare.append(gamePieceDiv);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -85,34 +90,31 @@ function endGame(msg) {
 function handleClick(evt) {
   // get x from ID of clicked cell
   let x = +evt.target.id;
-
-  clickCount++;
-  //alternate between player 1 and player 2
-  clickCount % 2 !== 0 ? (currPlayer = 1) : (currPlayer = 2);
-  //check if game is over
-  if (clickCount > 42) {
-    alert("Game is over!");
-  }
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
-
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
+
   placeInTable(y, x);
 
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-
+  if (board.every((row) => row.every((cells) => cells))) {
+    return endGame(`It's a tie!=`);
+  }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  //alternate between player 1 and player 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
+  console.log(currPlayer);
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -135,27 +137,27 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      let horiz = [
         [y, x],
         [y, x + 1],
         [y, x + 2],
         [y, x + 3],
       ];
-      var vert = [
+      let vert = [
         [y, x],
         [y + 1, x],
         [y + 2, x],
         [y + 3, x],
       ];
-      var diagDR = [
+      let diagDR = [
         [y, x],
         [y + 1, x + 1],
         [y + 2, x + 2],
         [y + 3, x + 3],
       ];
-      var diagDL = [
+      let diagDL = [
         [y, x],
         [y + 1, x - 1],
         [y + 2, x - 2],
