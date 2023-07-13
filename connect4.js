0dab974b0ae57514d760cc614ad86fb21f383ecb
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-07-12 20:46:24 -0400

    removed click count reasoning

--- connect4.js
+++ connect4.js
@@ -10,7 +10,6 @@ const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
-let clickCount = 0;
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
